c95db5932047db08460dbdf67d877a4f57174461
Ahmed-R <a.rguibi@gmail.com>
2015-08-26 12:16:59 +0200

    menu paths added

--- src/OC/FentisBundle/Controller/AdvertController.php
+++ src/OC/FentisBundle/Controller/AdvertController.php
@@ -16,13 +16,63 @@ class AdvertController extends Controller {
     public function indexAction(){
         $content = $this
                 ->get('templating')
-                ->render('OCFentisBundle:Advert:index.html.twig', array(
-                    'name' => 'Valkiard'
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "index"
                 ));
         return new Response($content);
     }
  
     public function charprofAction(){
-        return new Response("character profile page");
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "charprof"
+                ));
+        return new Response($content);
     }
-}
+
+    public function globalsAction(){
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "globals"
+                ));
+        return new Response($content);
+    }
+    
+    public function skillsAction(){
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "skills"
+                ));
+        return new Response($content);
+    }
+    
+    public function lifeinfoAction(){
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "lifeinfo"
+                ));
+        return new Response($content);
+    }
+    
+    public function equipAction(){
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "equip"
+                ));
+        return new Response($content);
+    }
+    
+    public function chatAction(){
+        $content = $this
+                ->get('templating')
+                ->render('OCFentisBundle:Advert:layout.html.twig', array(
+                        "name" => "chat"
+                ));
+        return new Response($content);
+    }
+    }
