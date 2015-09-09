4d662c5aa4020d81493768441a831b9c1679d715
Ahmed-R <a.rguibi@gmail.com>
2015-09-09 13:38:27 +0200

    code correction : /ajout and /voir work

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             
             new OC\FentisBundle\OCFentisBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
